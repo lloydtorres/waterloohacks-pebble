c04bdc37ccd6c49d3094f704ea8e6d44f54e37ee
Lloyd Torres <lloydtorres@users.noreply.github.com>
2016-01-23 09:39:48 -0500

    Create simple UI for testing

--- src/waterloohacks.c
+++ src/waterloohacks.c
@@ -4,17 +4,28 @@ Window *window;
 TextLayer *text_layer;
 
 void handle_init(void) {
+    Layer *window_layer;
+    GRect bounds;
+    
 	// Create a window and text layer
 	window = window_create();
-	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+    window_set_background_color(window, GColorTiffanyBlue);
+    
+    window_layer = window_get_root_layer(window);
+    bounds = layer_get_bounds(window_layer);
+    
+	text_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 	
 	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text(text_layer, "100%");
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer_set_background_color(text_layer, GColorClear);
+    text_layer_set_text_color(text_layer, GColorBlack);
+
 	
 	// Add the text layer to the window
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
 	// Push the window
 	window_stack_push(window, true);
