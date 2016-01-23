2c58a8dc174d87d4b6b9af3458c5caf34120d2b1
Lloyd Torres <lloydtorres@users.noreply.github.com>
2016-01-23 10:38:09 -0500

    Formatting stuff

--- src/waterloohacks.c
+++ src/waterloohacks.c
@@ -7,16 +7,18 @@ TextLayer *text_layer;
 void handle_init(void) {
     Layer *window_layer;
     GRect bounds;
+    int calc;
     
-	// Create a window and text layer
+	// Create a window
 	window = window_create();
     window_set_background_color(window, GColorCobaltBlue);
-    
     window_layer = window_get_root_layer(window);
     bounds = layer_get_bounds(window_layer);
     
-    mask_layer = bitmap_layer_create(GRect(0, 0, bounds.size.w, 50));
-    bitmap_layer_set_background_color(mask_layer, GColorWhite);
+    // Set up mask layer
+    calc = (bounds.size.h * 27) / bounds.size.h;
+    mask_layer = bitmap_layer_create(GRect(0, 0, bounds.size.w, calc));
+    bitmap_layer_set_background_color(mask_layer, GColorBlack);
     
     // Set the text, font, and text alignment
 	text_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -24,29 +26,25 @@ void handle_init(void) {
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
     text_layer_set_background_color(text_layer, GColorClear);
-    text_layer_set_text_color(text_layer, GColorBlack);
+    text_layer_set_text_color(text_layer, GColorWhite);
 	
-	// Add the text layer to the window
-	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+	// Add the mask and text layer to the window
     layer_add_child(window_layer, bitmap_layer_get_layer(mask_layer));
+	layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
 	// Push the window
 	window_stack_push(window, true);
 	
-	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+	//APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
 void handle_deinit(void) {
-	// Destroy the text layer
 	text_layer_destroy(text_layer);
-    
     bitmap_layer_destroy(mask_layer);
-	
-	// Destroy the window
 	window_destroy(window);
 }
 
+// Main app loop
 int main(void) {
 	handle_init();
 	app_event_loop();
